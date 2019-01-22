e4599495b2e0b6206792e63764f6537b45c18b40
David Mungin <david.mungin@ge.com>
2019-01-22 16:32:10 -0500

    updated angular cli added radial menu for status on press + hold fixed issue when scenario 14 was not shown as required by 31

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnChanges, ElementRef, Input, Output, EventEmitter, ViewChild, AfterViewInit } from '@angular/core';
-import * as cytoscape from 'cytoscape';
+import { Component, OnChanges, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import cytoscape from 'cytoscape';
+import cxtmenu from 'cytoscape-cxtmenu';
+import { MatRadioGroupBase } from '@angular/material';
+cytoscape.use(cxtmenu);
 @Component({
   selector: 'app-tree',
   template: '<div id="cy" #cy class="flex-item"></div>',
@@ -9,6 +12,8 @@ export class TreeComponent implements OnChanges {
   @Input()  elements: any;
   @Input()  selectedScenario: any;
   @Output() selectScenario = new EventEmitter();
+  @Output() updateScenario = new EventEmitter<any>();
+
   @ViewChild('cy') cyEl;
   private initialLoad: boolean = true;
   private cy: any;
@@ -30,65 +35,8 @@ export class TreeComponent implements OnChanges {
       pan = this.cy.pan();
       selectedNode = this.cy.nodes(':selected');
     }
-    this.cy = cytoscape({
-        container: this.cyEl.nativeElement,
-        elements: this.elements,
-        zoomingEnabled: false,
-        zoom: 0.5,
-        userZoomingEnabled: true,
-        boxSelectionEnabled: false,
-        autounselectify: false,
-        autolock: false,
-        layout: {
-          name: "preset"
-        },
-        style: cytoscape.stylesheet()
-          .selector('node')
-          .css({
-              'content': 'data(name)',
-              'font-size': '1.33em',
-              'font-weight': '600',
-              'text-valign': 'top',
-              'text-halign': 'center',
-              'color': '#000',
-              'text-outline-width': '0',
-              'background-color': '#000',
-              'text-outline-color': '#000',
-              'opacity': '.87',
-              'border-color': '#3f51b5',
-              'border-style': 'solid'
-          })
-          .selector('node[status = "locked"]')
-          .css({
-            'content': ( ele ) => '#' + ele.data('id')
-          })
-          .selector('edge')
-          .css({
-              'curve-style': 'bezier',
-              'target-arrow-shape': 'triangle',
-              'target-arrow-color': '#000',
-              'line-color': '#000',
-              'width': 2,
-              'opacity': '.87'
-          })
-          .selector('edge[type = "linksto"]')
-          .css({
-            'line-style': 'dashed'
-          })
-          .selector('edge[type = "requiredby"]')
-          .css({
-            'visibility': 'hidden',
-            'line-color': '#69f0ae',
-            'target-arrow-color': '#69f0ae'
-          })
-          .selector('edge[type = "blocks"]')
-          .css({
-            'visibility': 'hidden',
-            'line-color': '#f44336',
-            'target-arrow-color': '#f44336'
-          })
-
-    });
+    this.cy = cytoscape(this.getCytoscapeConfig());
+    this.cy.cxtmenu(this.getCxtMenuConfig());
     // Center the tree on initial load
     if (this.initialLoad) {
       pan = {x: (this.cy.width() / 2), y: 50};
@@ -108,6 +56,12 @@ export class TreeComponent implements OnChanges {
     this.setEdgeVisibility();
     this.colorScenarios();
     this.checkSpecialCases();
+    // selected nodes are pink
+    this.cy.nodes(':selected').css({
+      'color': '#ff4081',
+      'background-color': '#ff4081',
+      'border-width': '0px'
+    });
   }
   private setNodeVisibility() {
     this.cy.nodes('[status != "hidden"]')
@@ -129,8 +83,8 @@ export class TreeComponent implements OnChanges {
       .outgoers('edge[type = "unlocks"]')
       .css({'visibility': 'visible'});
     // Set requiredby edges from visible nodes to visible
-    this.cy.nodes('[status != "hidden"]')
-      .outgoers('edge[type = "requiredby"][target != "31"][target != "26"]')
+    this.cy.nodes('[status != "hidden"][id != 21]')
+      .outgoers('edge[type = "requiredby"][target != "26"]')
       .css({'visibility': 'visible'});
     // Set requiredby edges from complete nodes to hidden (requirement met)
     this.cy.nodes('[status = "complete"]')
@@ -165,15 +119,10 @@ export class TreeComponent implements OnChanges {
       'color': '#000',
       'background-color': '#fff',
       'border-width': '1px'});
-    // selected nodes are pink
-    this.cy.nodes(':selected').css({
-        'color': '#ff4081',
-        'background-color': '#ff4081',
-        'border-width': '0px'
-    });
+    
     // Scenarios blocked by other scenarios being incomplete are grey
-    this.cy.nodes('[status != "complete"]')
-      .outgoers('edge[type = "requiredby"][target != "31"][target != "26"]')
+    this.cy.nodes('[status != "complete"][id != 21]')
+      .outgoers('edge[type = "requiredby"][target != "26"]')
       .targets('node[status != "complete"]')
       .css({
         'background-color': '#c9c9c9',
@@ -186,8 +135,7 @@ export class TreeComponent implements OnChanges {
       .css({
         'background-color': '#f44336',
         'border-width': '0px'
-      })
-      ;
+      });
   }
   private checkSpecialCases() {
     let scenario21Complete = this.cy.nodes('#21').data('status') === 'complete';
@@ -299,4 +247,96 @@ export class TreeComponent implements OnChanges {
       }
     });
   }
+  private getCytoscapeConfig() {
+    return {
+      container: this.cyEl.nativeElement,
+      elements: this.elements,
+      zoomingEnabled: false,
+      zoom: 0.5,
+      userZoomingEnabled: true,
+      boxSelectionEnabled: false,
+      autounselectify: false,
+      autolock: false,
+      layout: {
+        name: "preset"
+      },
+      style: cytoscape.stylesheet()
+        .selector('node')
+        .css({
+            'content': 'data(name)',
+            'font-size': '18px',
+            'font-weight': '600',
+            'text-valign': 'top',
+            'text-halign': 'center',
+            'color': '#000',
+            'text-outline-width': '0',
+            'background-color': '#000',
+            'text-outline-color': '#000',
+            'opacity': '1',
+            'border-color': '#3f51b5',
+            'border-style': 'solid'
+        })
+        .selector('node[status = "locked"]')
+        .css({
+          'content': ( ele ) => '#' + ele.data('id')
+        })
+        .selector('edge')
+        .css({
+            'curve-style': 'bezier',
+            'target-arrow-shape': 'triangle',
+            'target-arrow-color': '#000',
+            'line-color': '#000',
+            'width': 2,
+            'opacity': '.87'
+        })
+        .selector('edge[type = "linksto"]')
+        .css({
+          'line-style': 'dashed'
+        })
+        .selector('edge[type = "requiredby"]')
+        .css({
+          'visibility': 'hidden',
+          'line-color': '#69f0ae',
+          'target-arrow-color': '#69f0ae'
+        })
+        .selector('edge[type = "blocks"]')
+        .css({
+          'visibility': 'hidden',
+          'line-color': '#f44336',
+          'target-arrow-color': '#f44336'
+        })
+    };
+  }
+  private getCxtMenuConfig() {
+    return {
+      commands: (ele) => {
+        let data = ele.data();
+        return [{
+        content: 'Incomplete',
+        fillColor: data.status === 'incomplete' ? 'rgba(255, 64, 129, 0.75)' : 'rgba(0, 0, 0, 0.75)',
+        select: (ele) => this.cxtMenuStatusChange('incomplete', ele)
+      }, {
+        content: 'Attempted',
+        fillColor: data.status === 'attempted' ? 'rgba(255, 64, 129, 0.75)' : 'rgba(0, 0, 0, 0.75)',
+        select: (ele) => this.cxtMenuStatusChange('attempted', ele)
+      }, {
+        content: 'Complete',
+        fillColor: data.status === 'complete' ? 'rgba(255, 64, 129, 0.75)' : 'rgba(0, 0, 0, 0.75)',
+        select: (ele) => this.cxtMenuStatusChange('complete', ele)
+      }]
+    },
+      activeFillColor: 'rgba(63, 81, 181, 1)'
+    };
+  }
+  private cxtMenuStatusChange(status, ele) {
+    let data = ele.data();
+    let scenario = {
+      id: data.id,
+      status: status,
+      notes: data.notes,
+      treasure: data.treasure
+    }
+    this.updateScenario.emit(scenario);
+  }
+
 }
